25f3916d06fbd76f101ab007c3d8d492490a7a22
bbk-pij-2012-20 <sjianz01@mail>
2013-12-27 13:37:39 +0000

    removed code of ContactManager that I'd put here initially by mistake. Moved a comment to FutureMeetingImpl.

--- ContactManagerEclipse/src/ContactManager/FutureMeeting.java
+++ ContactManagerEclipse/src/ContactManager/FutureMeeting.java
@@ -4,7 +4,4 @@ package ContactManager;
  * A meeting to be held in the future 
  */
 public interface FutureMeeting {
-	 // No methods here, this is just a naming interface
-
-	 // (i.e. only necessary for type checking and/or downcasting) 
 }
