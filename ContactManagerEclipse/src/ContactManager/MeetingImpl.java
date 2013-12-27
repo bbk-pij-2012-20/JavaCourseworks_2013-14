077f2252cabda267e6675b43e20d76a60af17802
bbk-pij-2012-20 <sjianz01@mail>
2013-12-27 15:23:52 +0000

    added 'implements' keyword. Constructor increments static id counter, initialises set of contacts,adds one contact,new method addContact(ContactImpl contact) allows more contacts to be added to a meeting one at a time.

--- ContactManagerEclipse/src/ContactManager/MeetingImpl.java
+++ ContactManagerEclipse/src/ContactManager/MeetingImpl.java
@@ -1,15 +1,37 @@
 package ContactManager;
 
-public class MeetingImpl {
+import java.util.Calendar;
+import java.util.Set;
+import java.util.HashSet;
+
+public class MeetingImpl implements Meeting {
+	
+	private static int id;
+	private Calendar date;
+	private Set<ContactImpl> contacts;
+	
+	// takes one contact at construction time. Therefore any meeting
+	// must always contain at least 1 contact. (No other constructor provided).
+	public MeetingImpl(Calendar date,ContactImpl contact) {
+		this.date = date;
+		contacts = new HashSet<>();
+		contacts.add(contact);
+		id++;
+	}
+	
+	public void addContact(ContactImpl contact) {
+		contacts.add(contact);
+	}
+	
 	public int getId() {
-		
+		return id;
 	}
 	
 	public Calendar getDate() {
-		
+		return date;
 	}
 	
 	public Set<Contact> getContacts() {
-		
+		return contacts;
 	}
 }
