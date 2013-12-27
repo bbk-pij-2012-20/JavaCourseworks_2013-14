282b3e518acb12afb74a877c1a7571cba4bfafde
bbk-pij-2012-20 <sjianz01@mail>
2013-12-27 15:19:01 +0000

    added addContact(Contact contact) method

--- ContactManagerEclipse/src/ContactManager/Meeting.java
+++ ContactManagerEclipse/src/ContactManager/Meeting.java
@@ -2,6 +2,7 @@ package ContactManager;
 
 import java.util.Calendar; 
 import java.util.Set;
+import java.util.HashSet;
 
 /**
  * A class to represent meetings 
@@ -9,17 +10,27 @@ import java.util.Set;
  * Meetings have unique IDs, scheduled date and a list of participating contacts 
  */
 public interface Meeting {
+	
+	/**
+	 * (Not in original interface). 
+	 * Add contacts to the meeting which takes one 
+	 * contact at construction time.
+	 * 
+	 * @param	contact		the contact added one-by-one to a meeting.
+	 */
+	 void addContact(ContactImpl contact);
+	
 	/**
 	 * Returns the id of the meeting.
 	 *
-	 * @return the id of the meeting.
+	 * @return	the id of the meeting.
 	 */
 	 int getId();
 
 	/**
 	 * Return the date of the meeting. 
 	 *
-	 * @return the date of the meeting. 
+	 * @return	the date of the meeting. 
 	 */
 	 Calendar getDate();
 
@@ -30,7 +41,7 @@ public interface Meeting {
 	 * just two people: the user and the contact) and may contain an
 	 * arbitrary number of them. 
 	 *
-	 * @return the details of people that attended the meeting. 
+	 * @return	the details of people that attended the meeting. 
 	 */
 	 Set<Contact> getContacts(); 
 }
