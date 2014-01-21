178c4868276a15a484e4383340b033b9f336f2a1
bbk-pij-2012-20 <sjianz01@mail>
2014-01-21 00:47:40 +0000

    Contact tests completed.

--- ContactManagerEclipse/src/ContactManager/ContactImplTest.java
+++ ContactManagerEclipse/src/ContactManager/ContactImplTest.java
@@ -6,34 +6,53 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class ContactImplTest {
+	private ContactImpl contact;
+	private String name;
+	private int id;
+	private String note;
 	
 	@Before
 	public void setUp() throws Exception {
+		id = 1234567;
+		name = "Shahin Zibaee";
+		note = "is a football guru";
+		contact = new ContactImpl(name,note,id);
 	}
 	
 	@After
 	public void tearDown() throws Exception {
-//		ContactImpl ci = null;
+		contact = null;
 	}
 
 	@Test
 	public void testGetId() {
-		new ContactImpl("Shahin Zibaee");
-		new ContactImpl("Shahin Zibaee2");
-		new ContactImpl("Shahin Zibaee3");
-		assertEquals(3,ContactImpl.getId());		
+		int expectedOutput = id;
+		int actualOutput = contact.getId();
+		assertEquals(expectedOutput,actualOutput);
 	}
 
 	@Test
-	public void testGetName() {
-		ContactImpl ci = new ContactImpl("Shahin Zibaee");
-		assertEquals("Shahin Zibaee",ci.getName());
+	public void testGetName() {		
+		String expectedOutput = name;
+		String actualOutput = contact.getName();
+		assertEquals(expectedOutput,actualOutput);
+	}
+
+	@Test
+	public void testGetNotes() {		
+		contact = new ContactImpl(name,note,id);
+		String expectedOutput = note;
+		String actualOutput = contact.getNotes();
+		assertEquals(expectedOutput,actualOutput);
 	}
 
 	@Test
 	public void testAddNotes() {		
-		ContactImpl ci = new ContactImpl("Shahin Zibaee");
-		ci.addNotes("..is a football god");
-		assertEquals("..is a football god",ci.getNotes());
+		contact = new ContactImpl();
+		contact.addNotes(note);
+		String expectedOutput = note;
+		String actualOutput = contact.getNotes();
+		assertEquals(expectedOutput,actualOutput);
 	}
+
 }
