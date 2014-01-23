897576cdc7710e09b7416cd81f53e5e88e8c0eca
bbk-pij-2012-20 <sjianz01@mail>
2014-01-23 22:03:45 +0000

    trying to debug the test/code for getPastMeeting() to work and generateId() to work properly.

--- ContactManagerEclipse/src/ContactManager/ContactManagerImplTest.java
+++ ContactManagerEclipse/src/ContactManager/ContactManagerImplTest.java
@@ -6,94 +6,124 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.lang.reflect.Method;
 import java.util.GregorianCalendar;
 import java.util.Calendar;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.Map;
+import java.util.HashMap;
 
 public class ContactManagerImplTest {
 	
-	Calendar someDate;
-	Calendar nowDate;
-	ContactManagerImpl cmi = null;
-	Set<Contact> someContacts;
-	int counter = 0;
-
+	private Calendar somePastDate;
+	private Calendar someFutureDate;
+	private Set<Contact> someContacts;//NEED TO CHANGE TO LIST BECAUSE CONTACTS CAN HAVE IDENTICAL NAMES!
+	private ContactManagerImpl cmi; // *
+	private String someText;
+	private int pastMeetingId;
+	private int futureMeetingId;
+	private int contactId;
+	private PastMeeting pastMeeting;
+	private FutureMeeting futureMeeting;
+	private Map<Integer,PastMeeting> pastMeetingMap;
+	
+//	*NB: cmi needs to be the Impl because toString(date) and 
+//	generateId() are not in the interface.
+	
 	@Before
 	public void setUp() throws Exception {
 		cmi = new ContactManagerImpl();
+		pastMeeting = new PastMeetingImpl();
+		pastMeetingMap = new HashMap<>();
+		someFutureDate = new GregorianCalendar(2014,11,15);		
+//		somePastDate = Calendar.getInstance();
+		somePastDate = new GregorianCalendar(1974,11,15);
+//		System.out.println("in @Before of cmiTest - date: "+somePastDate.getTime());
+		
+		someText = "I need a haircut";
+		someContacts = new HashSet<>();
+		someContacts.add(new ContactImpl("bob"));
+		someContacts.add(new ContactImpl("mac"));
+		someContacts.add(new ContactImpl("mac"));
 	}
 
 	@After
 	public void tearDown() throws Exception {
+//		cmi = null;
 	}
 
-	@Before
-	public void setUp2() throws Exception {
-		someDate = new GregorianCalendar(15,12,2014);
-		nowDate = Calendar.getInstance();
-		someContacts = new HashSet<>();
-		someContacts.add(new ContactImpl("bob"));
-		someContacts.add(new ContactImpl("mac"));
-		someContacts.add(new ContactImpl("mac"));		
-	}
-	/**
-	 * test for addfutureMeeting, takes a set of contacts and a calendar date
-	 * and generates unique id using 'generateId()' and date as a string.
-	 */
 	@Test
-	public void testAddFutureMeeting() {		
-		assertEquals(2,cmi.addFutureMeeting(someContacts, someDate));
+	public void testAddFutureMeeting() {
+//		int expectedOutput = 975771; // id for a past date 15/12/74, would cause exception, as it should.
+		int expectedOutput = 328271;
+//		int actualOutput = cmi.addFutureMeeting(someContacts,somePastDate);//would cause exception, as it should.
+		int actualOutput = cmi.addFutureMeeting(someContacts,someFutureDate);
+		assertEquals(expectedOutput,actualOutput);
 	}
 	
 	/**
-	 * Unfortunately had to resort to changing accessibility of generateId() to public.  
-	 * (My failed attempt at using reflection to make private method generateId()  
-	 * accessible to JUnit test is commented out). 
-	 *
+	 * testing generateId() with name string inputs
 	 */
 	@Test
-	public void testGenerateId() {
-/*		String parameters;
-		Method method = ContactManagerImpl.getDeclaredMethod(cmi.generateId("bob"), parameters);/
-		method.setAccessible(true);
-		int result = method.invoke(cmi, argObjects);*/
+	public void testGenerateId() {		
+		int expectedOutput1 = 977170;
+		int expectedOutput2 = 78550;
+		int expectedOutput3 = 78551;
+		int actualOutput1 = cmi.generateId("bob");		
+		int actualOutput2 = cmi.generateId("mac");
+		assertEquals(expectedOutput1,actualOutput1);
+		assertEquals(expectedOutput2,actualOutput2);
+	}
+
+	/**
+	 * testing generateId() with Calendar date inputs
+	 */
+	@Test
+	public void testGenerateId2() {
+	int expectedOutput1 = 328271;//future date 2014,11,15
+	int expectedOutput2 = 975771;//past date 1974,11,15
+	System.out.println("toString(futureDate)"+cmi.toString(someFutureDate));
+	
+	int actualOutput1 = cmi.generateId(cmi.toString(someFutureDate));
+	int actualOutput2 = cmi.generateId(cmi.toString(somePastDate));
+
+	assertEquals(expectedOutput1,actualOutput1);
+	assertEquals(expectedOutput2,actualOutput2);
+	}
+	
+	@Test
+	public void testGetPastMeeting() {				
+		for (Contact contact : someContacts) {
+			System.out.println("someContacts: "+contact.getName());
+		}
+		cmi.addNewPastMeeting(someContacts,somePastDate,someText);//*if this is commented out
+		pastMeetingMap.put(975771,pastMeeting);//*and this is commented out
+		System.out.println("somePastDate: "+pastMeeting.getDate());
 		
-		int expectedOutput1 = 977171;
-		int expectedOutput2 = 78552;
-		int expectedOutput3 = 78553;
-		assertEquals(expectedOutput1,cmi.generateId("bob"));
-		assertEquals(expectedOutput2,cmi.generateId("mac"));
-		assertEquals(expectedOutput3,cmi.generateId("mac"));
+		int actualOutput = 0;
+		for (Map.Entry<Integer,PastMeeting> entry : pastMeetingMap.entrySet()) {
+			if (entry.getValue().equals(pastMeeting)) {
+				actualOutput = entry.getKey();
+			}
+		}
+		int expectedOutput = 975771;
+//		int expectedOutput = 0; //* then this would pass.
+		assertEquals(expectedOutput,actualOutput);	
 	}
-	
-/*	@Test
-	public void testGetPastMeeting() {
-		ContactManagerImpl cmi = new ContactManagerImpl();
-		PastMeetingImpl pmi = new PastMeetingImpl(contacts,date);
-		assertEquals(pmi,cmi.getPastMeeting(-1));	
-	}*/
 
 /*	@Test
 	public void testGetFutureMeeting() {
-		ContactManagerImpl cmi = new ContactManagerImpl();
-		FutureMeetingImpl fmi = new FutureMeetingImpl(contacts,date);
-		assertEquals(fmi,cmi.getFutureMeeting(2));	
+		
 	}*/
 
 /*	@Test
 	public void testGetMeeting() {
-		ContactManagerImpl cmi = new ContactManagerImpl();
-		MeetingImpl mi = new MeetingImpl();
-		assertEquals(mi,cmi.getMeeting(1));
+		
 	}*/
 
 	/*	@Test
 	public void testGetFutureMeetingListContactImpl() {
-		ContactManagerImpl cmi = new ContactManagerImpl();
-		FutureMeetingImpl fmi = new FutureMeetingImpl();
-		assertEquals(mi,cmi.getFutureMeeting(4));
+		
 	} */
 
 	@Test
@@ -106,50 +136,43 @@ public class ContactManagerImplTest {
 		fail("Not yet implemented");
 	}
 
+	/**
+	 * In order to test this method, it is first called,
+	 * with some contacts, a past date and text.
+	 * I know the past date produces an int Id of 975771. 
+	 * So, in order to test for creation of the new meeting,
+	 * partly confirmed by the creation of the id, the key-
+	 * value pair is put in the map, and finally, the id 
+	 * for the new meeting created is compared with the 
+	 * expected id number (975771).
+	 */
 	@Test
 	public void testAddNewPastMeeting() {
-		fail("Not yet implemented");
+		cmi.addNewPastMeeting(someContacts,somePastDate,someText);//*if this is commented out
+		pastMeetingMap.put(975771,pastMeeting);//*and this is commented out
+		int actualOutput = 0;
+		for (Map.Entry<Integer,PastMeeting> entry : pastMeetingMap.entrySet()) {
+			if (entry.getValue().equals(pastMeeting)) {
+				actualOutput = entry.getKey();
+			}
+		}
+		int expectedOutput = 975771;
+//		int expectedOutput = 0; //* then this would pass.
+		assertEquals(expectedOutput,actualOutput);	
 	}
 
 	@Test
 	public void testAddMeetingNotes() {
 		fail("Not yet implemented");
 	}
-
-/*	@Before
-	public void setUp() throws Exception {
-		ContactManagerImpl cmi = new ContactManagerImpl();
-		cmi.contactCounter = 0;
-	}*/
-/*	@Test
+/*	
+ 	@Test
 	public void testAddNewContact() {
-		ContactImpl c1 = new ContactImpl();		
-		String name = "Shahin";
-		ContactManagerImpl cmi = new ContactManagerImpl();
-		cmi.addNewContact(name,"hash");
-		int contactCounter = 1;
-		Long hashId = (long) Math.abs(name.hashCode());
-		int hashIdInt = (int) (hashId%100000);
-		String idStr = ""+hashIdInt+contactCounter;
-		int idTest = Integer.parseInt(idStr);
-		assertEquals(idTest,c1.getId());
-	}*/
-
-	@Before
-	public void setUp12() throws Exception {
-		contacts = new HashSet<>();
-		contacts.add(new ContactImpl("bob"));
-		contacts.add(new ContactImpl("mac"));
-		contacts.add(new ContactImpl("mac"));	
-		int id = 977171;
-		int id = 78552;
-		int id = 78553;
-		
+	}
+*/
 		
 	@Test
 	public void testGetContactsIntArray() {
-		
-		assertEquals(contacts,);
 	}
 
 	@Test
