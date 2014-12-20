a438e922972b6b6017774cae140baf9e6606bca7
SZibaee <sjianz01@mail.bbk.ac.uk>
2014-12-20 14:56:52 +0000

    deleting to start again

--- ContactManagerEclipse/src/ContactManager/FutureMeeting.java
+++
@@ -1,9 +0,0 @@
-package ContactManager;
-
-/**
- * A meeting to be held in the future
- * No methods here, this is just a naming interface
- * (i.e. only necessary for type checking and/or downcasting)  
- */
-public interface FutureMeeting extends Meeting {
-}
